2e238ee35751f94394a52274c8678c7b79850580
rprasetya <rayaprasetya90@gmail.com>
2023-05-14 09:51:57 +0800

    update db

--- pa_web.sql
+++ pa_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2023 at 09:27 AM
+-- Generation Time: May 14, 2023 at 03:50 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -46,6 +46,52 @@ INSERT INTO `data_user` (`id`, `email`, `username`, `password`, `roles`) VALUES
 (6, '123', '123', '$2y$10$/r0sMdVxJTEiZj4hjpm1IeWW2iX5gYWLHmELqH1sLWvsC/lZbjSS6', 'user'),
 (7, 'qwe', 'qwe', '$2y$10$MC5Q.qscF6Ws65Y3uw7uhOV5fmspT1hFUBx6HN2ublOY.ZNZNnCu2', 'user');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(2) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `image` varchar(40) NOT NULL,
+  `price` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `name`, `image`, `price`) VALUES
+(1, 'Kue 1', '1.PNG', 200000),
+(2, 'kue 2', '2.PNG', 300000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_user`
+--
+
+CREATE TABLE `order_user` (
+  `id` int(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `orderan` varchar(255) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `hargaSatuan` int(255) NOT NULL,
+  `kuantitas` int(255) NOT NULL,
+  `totalHarga` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_user`
+--
+
+INSERT INTO `order_user` (`id`, `user`, `image`, `orderan`, `status`, `hargaSatuan`, `kuantitas`, `totalHarga`) VALUES
+(23, 'user1', 'image/1.PNG', 'Kue 1', 'Verified', 200000, 1, 200000),
+(24, 'user1', 'image/2.PNG', 'kue 2', 'Verified', 300000, 2, 600000);
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +103,18 @@ ALTER TABLE `data_user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_user`
+--
+ALTER TABLE `order_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +123,18 @@ ALTER TABLE `data_user`
 --
 ALTER TABLE `data_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `order_user`
+--
+ALTER TABLE `order_user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
